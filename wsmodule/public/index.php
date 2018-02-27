6878c5c6ac8e029f2455d68cd53a90272d75985e
master-genius <1146040444@qq.com>
2018-02-27 23:15:20 +0800

    update dir

--- wsmodule/public/index.php
+++ wsmodule/public/index.php
@@ -1,24 +1,50 @@
 <?php
 require '../vendor/autoload.php';
 
-// 定义系统常量
-define('APP_PATH', __DIR__ . '/');
-define('ROOT_PATH', __DIR__ . '/../');
+//use Medoo\Medoo;
+//use bravemaster\core\apicall;
+use bravemaster\action\wsuser;
 
+define('ROOT_PATH', dirname(__FILE__) . '/');
+define('APP_PATH', dirname(ROOT_PATH) );
+
+$api = new apicall;
+
+$config = [];
+
+$config['wsuser'] = new wsuser;
 // 实例化 App 对象
 $app = new \Slim\App($config);
 
-
-// 添加路由回调, 默认显示index.html
-$app->get('/', function ($request, $response, $args) {
-
+$app->get('/login', function($request, $response, $args){
+    $r = $this->get('wsuser')->login();
+    return $response->withHeader('Access-Control-Allow-Origin','*')
+                    ->withStatus(200)
+                    ->write(json_encode($r));
 });
 
-$app->get('/phpinfo',function($request,$response,$args){
-    
+$app->get('/register', function($request, $response, $args){
+    $r = $this->get('wsuser')->register();
+    return $response->withHeader('Access-Control-Allow-Origin','*')
+                    ->withStatus(200)
+                    ->write(json_encode($r));
 });
 
+/*
+$app->any('/api/{call}/{action}', function($request, $response, $args){
+    $r = $this->get('apicall')
+            ->dispatch($args['call'],$args['action'],$request,$response);
+    return json_encode($r, JSON_UNESCAPED_UNICODE);
+})->add(function($request, $response, $next){
+    $user_id = request_data('get', 'user_id', '');
+    $api_token = request_data('get', 'api_token', '');
 
-// 运行应用
+    $r = apiauthorise($user_id, $api_token);
+
+    $response = $next($request, $response);
+    return $response;
+});
+*/
+
 $app->run();
 
